1f5bf8d34b9ef3330e947abc23a1f11982153c78
mirend2002 <mirend2002@yahoo.com>
2016-09-08 13:41:14 +0530

    bug fixes in delete validation
    bug fixes in delete validation

--- dbValidator/util.js
+++ dbValidator/util.js
@@ -1,11 +1,11 @@
 db = require('./DBManager'),
 validator = require('./validator');
 
-   // if(validator.insertDeviceAPI("user",["name", "prefix", "users"],["miren", "miren2002", "2999999"]) == true){
-   //     console.log("working");
-   // }else{
-   //     console.log("not working");
-   // }
+/**if(validator.insertDeviceAPI("user",["name", "prefix", "users"],["miren", "miren2002", "2999999"]) == true){
+        console.log("working");
+    }else{
+        console.log("not working");
+    }*/
 
     if(validator.deleteDeviceAPI("user",["name"],["miren"]) == true){
           console.log("working");
--- dbValidator/validator.js
+++ dbValidator/validator.js
@@ -108,6 +108,58 @@ exports.deleteDeviceAPI = function deleteDeviceAPI(collection,document,data) {
         //create db schema model
         var object = mongoose.model(collection.capitalizeFirstLetter(),schemaVal);
 
+        //object.remove(eval(JSON.parse(data))).exec();
+        object.findOne(eval(JSON.parse(data)), function (error, data){
+            console.log(data + "\nDocument is deleted successfully");
+            data.remove();
+
+        });
+    }
+}
+
+
+exports.updateDeviceAPI = function updateDeviceAPI(collection,document,data) {
+
+    var jasonFileName="./DeviceAPI/"+collection+"Sechema.json";
+    // Get content from file
+    var contents = fs.readFileSync(jasonFileName);
+    // Define to JSON type
+    var jsonContent = JSON.parse(contents);
+
+
+    var json="jsonContent.";
+    if (eval("jsonContent."+collection)  === undefined) {
+        console.log("Invalid Collection :"+collection);
+        return false;
+    }else{
+        var record="";
+        var schemaRecord="";
+        for (i = 0; i < document.length; i++) {
+            var val=false;
+
+            for (var key in eval("jsonContent."+collection)) {
+                if(key === document[i]){
+                    record+=' "'+document[i]+'": "'+data[i]+'",';
+                    schemaRecord+=' "'+document[i]+'": "'+eval("jsonContent."+collection+"."+document[i])+'",';
+                    val=true;
+                }
+            }
+            if(val === false){
+                console.log("Invalid Document :"+document[i]);
+                return false;
+            }
+        }
+
+        var data = "{ "+record.replace(/,\s*$/, "")+" }";
+        //create db schema
+        var schemaData = "{ "+schemaRecord.replace(/,\s*$/, "")+" }";
+        var schemaVal = Schema(
+            eval(JSON.parse(schemaData))
+        );
+
+        //create db schema model
+        var object = mongoose.model(collection.capitalizeFirstLetter(),schemaVal);
+
         object.remove(eval(JSON.parse(data))).exec();
     }
 }
